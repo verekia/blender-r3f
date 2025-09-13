0a935dbf4d73c775c079777ee3185e3575258190
verekia <522007+verekia@users.noreply.github.com>
2025-09-13 16:00:30 +0200

    feat: add glb cube and plane

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,25 @@
-import { OrbitControls } from "@react-three/drei"
+import { OrbitControls, PerspectiveCamera, useGLTF } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
+
+const Cube = () => {
+  const { scene } = useGLTF("./cube.glb")
+
+  return <primitive object={scene} />
+}
+
+const Plane = () => {
+  const { scene } = useGLTF("./plane.glb")
+  return <primitive object={scene} />
+}
 
 const IndexPage = () => (
   <Canvas>
-    <OrbitControls />
-    <mesh>
-      <boxGeometry />
-      <meshStandardMaterial color="red" />
-    </mesh>
-    <directionalLight position={[4, 4, 4]} intensity={1} />
-    <directionalLight position={[4, 4, -4]} intensity={1} />
+    <OrbitControls>
+      <PerspectiveCamera makeDefault position={[0, 4, 10]} />
+    </OrbitControls>
+    <Cube />
+    <Plane />
+    <directionalLight position={[4, 4, 4]} intensity={2} />
     <ambientLight intensity={1} />
   </Canvas>
 )
